6e43cd014c6c846625625ef58806cb0ae98dfa07
nancy nyokabi <nancynyokabi1995@gmail.com>
2022-01-25 10:09:44 +0300

    created a unittest class that defines cases for the user class

--- user-test.py
+++ user-test.py
@@ -1,3 +1,13 @@
 import unittest
 from collections import UserList
 from user import Details, User
+
+class TestClass(unittest.TestCase):
+    """
+    A Test class that defines test cases for the User class.
+    """
+    def setUp(self):
+        """
+        Method that runs before each individual test methods run.
+        """
+        self.new_user = User('NancyNdungu','XyZ3thf1')
