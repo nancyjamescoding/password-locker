75f78df504110441bf252ae39180da834ee4a052
nancy nyokabi <nancynyokabi1995@gmail.com>
2022-01-25 10:24:36 +0300

    Created a test case to check if the object is saved

--- user-test.py
+++ user-test.py
@@ -37,6 +37,19 @@ class TestDetails(unittest.TestCase):
         Test case to check if a new Credentials instance has been initialized correctly
         """
         self.assertEqual(self.new_credential.account,'Gmail')
-        self.assertEqual(self.new_credential.userName,'Owiti_Charles')
+        self.assertEqual(self.new_credential.userName,'nancy_ndungu')
         self.assertEqual(self.new_credential.password,'yx5Gij43')
+    
+    def save_credential_test(self):
+        """
+        test case to test if the crential object is saved into the credentials list.
+        """
+        self.new_credential.save_details()
+        self.assertEqual(len(Details.details_list),1)
+
+    def tearDown(self):
+        '''
+        method that does clean up after each test case has run.
+        '''
+        Details.details_list = []    
             
